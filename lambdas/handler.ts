cdc7ce1ce3977ed8ae79fba9a8cd3e65a02631b6
novellito <novellito7@gmail.com>
2020-02-09 20:49:11 -0800

    Implement initial FE/BE integrations:    - Add .env & snoowrap dependency to BE

--- lambdas/handler.ts
+++ lambdas/handler.ts
@@ -1,21 +1,37 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+const snoowrap = require('snoowrap');
+// import snoowrap from 'snoowrap';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello: APIGatewayProxyHandler = async (event: any, _context) => {
+  //event.body --> has key
+  const body = JSON.parse(event.body);
+
+  const { accessToken } = await snoowrap.fromAuthCode({
+    code: body.code,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+    userAgent: 'user agent here!'
+  });
+  const r = new snoowrap({
+    userAgent: 'user agent here!',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    accessToken
+  });
+
+  const elem = await r.getMe().getSavedContent();
+  console.log(elem);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message:
-          'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-        input: event
+        savedPosts: elem
+        // savedPosts: elem
       },
       null,
       2
     )
   };
 };
-
-// snoowrap.fromAuthCode({ code: event.data.code, clientId, clientSecret, redirectUri }).then(userSnoowrap => {
-//   this.setState({ userSnoowrap, displayedAccessToken: userSnoowrap.accessToken });
-// });
