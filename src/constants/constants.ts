976d5c73a70271ca3a6675e5aa9130add6b8cf34
novellito <novellito7@gmail.com>
2020-09-05 23:20:17 -0700

    remove unneeded reddit scope

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,8 +1,3 @@
 export const ENDPOINT_DOMAIN = 'reddit.com';
-export const REDDIT_SCOPE = [
-  'identity',
-  'edit', //?
-  'save', //?
-  'history'
-];
+export const REDDIT_SCOPE = ['identity', 'save', 'history'];
 export const CLIENT_ID = 'Kit8DCMzQX8K1Q';
